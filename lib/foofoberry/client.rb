e6f5749f78f816dda56e7107ca7ce6b3ce908c82
Tyler Long <tyler.stephen.long@gmail.com>
2014-01-13 18:29:17 -0700

    Fix indentation problems
    Spacing was wrong

--- lib/foofoberry/client.rb
+++ lib/foofoberry/client.rb
@@ -1,14 +1,18 @@
 module FooFoBerry
   class Client
+    
     def connection
       @connection ||= new_connection
     end
+    
     def new_connection
       Faraday.new(:url => 'http://localhost:3001/')
     end
+    
     def get(path)
       connection.get(path + extension)
     end
+    
     def post(path, body)
       connection.post do |req|
         req.url path
@@ -16,6 +20,7 @@ module FooFoBerry
         req.body = body
       end
     end
+    
     def extension
       ".json"
     end
