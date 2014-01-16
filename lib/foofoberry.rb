93b878598786939d5c07d8346cf156c7f10ee371
Simon Taranto <simon.taranto@gmail.com>
2014-01-16 00:38:15 -0700

    Add project_store file to require statements

--- lib/foofoberry.rb
+++ lib/foofoberry.rb
@@ -2,6 +2,7 @@ require "foofoberry/version"
 require "foofoberry/client"
 require "foofoberry/github_notification"
 require "foofoberry/project"
+require "foofoberry/project_store"
 
 module Foofoberry
   # Your code goes here...
