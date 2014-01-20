da6d62d69c7788ce2eb8089c29c84acc8a8f8e63
Nathaniel Watts <reg@nathanielwatts.com>
2014-01-20 15:26:59 -0700

    setup correct Repo API endpoint

--- lib/foofoberry/repo.rb
+++ lib/foofoberry/repo.rb
@@ -10,7 +10,10 @@ module FooFoBerry
     end
 
     def create_with(params)
-      response = client.post("repos", params.to_json)
+      url  = "projects/#{params[:repo][:project_id]}/repos"
+      body = { :repo => { :github_url => params[:repo][:github_url] } }
+
+      response = client.post(url, body.to_json)
       [response.status, JSON.parse(response.body)]
     end
   end
