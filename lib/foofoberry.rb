a146b504fe49f409c6bced8459dfd14bffc65fc0
Nathaniel Watts <reg@nathanielwatts.com>
2014-01-20 11:53:21 -0700

    remember to include the repo class on the base file

--- lib/foofoberry.rb
+++ lib/foofoberry.rb
@@ -3,6 +3,7 @@ require "foofoberry/client"
 require "foofoberry/github_notification"
 require "foofoberry/project"
 require "foofoberry/project_store"
+require "foofoberry/repo"
 
 module Foofoberry
   # Your code goes here...
